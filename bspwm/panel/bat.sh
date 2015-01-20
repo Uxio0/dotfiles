923d5ad5fdcd59779b01969b520010aeaef5f7c8
Uxío <ufuentefria@telecongalicia.com>
2015-01-20 16:45:47 +0100

    Added bspwm configs

---
+++ bspwm/panel/bat.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source $(dirname $0)/config.sh
+
+FULL=`acpi -b | awk '{gsub(/%,/,""); print $4}' | sed 's/%//g'`
+NOTFULL=`acpi -b | awk '{ split($5,a,":"); print substr($3,0,2), $4, "["a[1]":"a[2]"]" }' | tr -d ',' | sed -e 's/\[//g;s/\]//g'`
+
+[[ $FULL -eq 100 ]] && echo -e "%{F$THECOLOR}${BATTIC}" || echo -e "%{F$THECOLOR}%{F$FOREG} $NOTFULL"
---
+++ bspwm/panel/config.sh
@@ -0,0 +1,35 @@
+# Colors declaration
+
+FOREG='#FF797979'
+FGM='#FFAE2841'
+WIRE='#FF424242'
+BATTERY='#FF2A4649'
+THECOLOR='#FF3F4B9C'
+BLUE='#FFAA9A4B'
+YELLOW='#FFBABE3F'
+
+. panel_colors
+
+# Info used in bar
+# Icons declarations
+
+i_pkgs='\ue14d'   # pacman
+i_mail='\ue072'     # mail icon
+i_wifi='\ue0f0'     # signal
+i_time='\ue017'     # clock
+i_load='\ue021'     # micro chip
+i_musk='\ue04d '     # headphones
+i_alsa='\ue05d'     # speaker
+i_tmp='  '
+i_mpd=' '
+i_bat=' '
+
+# Call those motherfuckers icons
+BATTIC=`echo -e $i_bat`
+WIFIIC=`echo -e $i_wifi`
+SOUNDIC=`echo -e $i_alsa`
+MEMIC=`echo -e $i_load `
+MUSIC=`echo -e $i_musk`
+TEMPIC=`echo -e $i_tmp`
+PACIC=`echo -e $i_pkgs`
+CLKIC=`echo -e $i_time`
