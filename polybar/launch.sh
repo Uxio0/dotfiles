a1a9ad71d922ffe729c9ab7fb8e0a448d59a850c
Uxío <uxio11@gmail.com>
2020-02-19 15:03:58 +0100

    Update bspwm

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,6 +7,8 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar bar1 &
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload bar1 &
+done
 
 echo "Bars launched..."
